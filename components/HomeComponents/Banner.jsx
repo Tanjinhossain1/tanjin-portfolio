6c6d17d18984bd24d2f025dc48cacca66590b8c5
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-03-17 00:57:02 +0600

    update loading screan

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
                     <div className="flex items-center md:items-center md:justify-around">
                         <div className="">
                             <div className="">
-                                <h1 className="text-3xl sm:text-4xl xl:text-5xl text-gray-300 font-bold">Hello!</h1>
+                                <h1 className="text-3xl sm:text-4xl xl:text-5xl text-gray-300 font-bold"><span className='text-pink-600'>H</span>ello<span className='text-pink-600'>!</span></h1>
                             </div>
                             <div className="">
                                 <div className=" py-4 font-cascadia-normal text-Snow pb-4 text-xs h-20 lg:h-auto">
